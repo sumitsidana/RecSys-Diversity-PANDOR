45e324b1725a37c76841cebef6f1f71c5768065c
sumit <sumit.sidana@imag.fr>
2017-12-01 10:30:52 +0100

    add: run rank als

--- input_metrics.sh
+++ input_metrics.sh
@@ -1,8 +1,9 @@
-directory="/data/sidana/diversity/purch/data"
 mkdir -p /data/sidana/diversity/purch/data/validusers
 mkdir -p /data/sidana/diversity/purch/data/nonvalidusers
-for filename in "$directory/divrankals_*";
+for filename in divrankals_*;
 do
+	echo `wc -l "$filename"`
 	grep -v "0.0$" "${filename}" > "validusers/${filename}"
 	grep "0.0$" "${filename}" > "nonvalidusers/${filename}"
+	echo `wc -l "validusers/${filename}" "nonvalidusers/${filename}"`
 done
